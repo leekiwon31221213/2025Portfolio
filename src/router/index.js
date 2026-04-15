090bcb000b51b11bb432b3fb70db14c44ee14ea2
Lee Ki Won <cute01235@nate.com>
2026-04-15 15:32:55 +0900

    라우터 경로 수정

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 // 페이지 컴포넌트 가져오기
 import HomeView from '/src/views/HomeView.vue'
-import GsttDetail from '/src/views/GsttDetail.vue'
+import GsttDetail from '/src/views/gstt/GsttDetail.vue'
 
 // 라우터 목록
 const routes = [
