a1aef2268bcd010ad29b23406c7f41399cd81621
Lee Ki Won <cute01235@nate.com>
2026-04-15 15:14:27 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 // 페이지 컴포넌트 가져오기
-import GsttDetail from '/src/views/gstt/GsttDetail'
+import GsttDetail from '@/views/gstt/GsttDetail.vue'
 
 // 라우터 목록
 const routes = [
